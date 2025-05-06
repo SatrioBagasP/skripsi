618181e7af3d42a64b0e659f4cb3134189f4f06c
SatrioBagasPangestu <navi.squishy.23@gmail.com>
2025-05-06 20:30:33 +0700

    ✨ Add: Initial Migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            // $table->foreignId('role_id')->nullable()->constrained('role')->nullOnDelete();
+            $table->unsignedBigInteger('userable_id')->nullable();
+            $table->string('userable_type')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
---
+++ database/migrations/2025_05_03_065633_create_proposal_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proposal', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('dosen_id')->constrained('dosen');
+            $table->string('no_proposal')->unique();
+            $table->string('name');
+            $table->text('desc');
+            $table->string('file');
+            $table->boolean('is_harian');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->text('alasan_tolak')->nullable();
+            $table->boolean('is_acc_dosen')->nullable();
+            $table->boolean('is_acc_kaprodi')->nullable();
+            $table->boolean('is_acc_minat_bakat')->nullable();
+            $table->boolean('is_acc_layanan')->nullable();
+            $table->boolean('is_acc_wakil_rektor')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proposal');
+    }
+};
