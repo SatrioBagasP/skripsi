1357e763d08658c2957bdcd7e08899a7cf13fd6e
SatrioBagasPangestu <navi.squishy.23@gmail.com>
2025-06-18 19:41:08 +0700

    ✨ Add: Feat Modul #1 - Added auth login logout

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Models\Dosen;
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function index()
+    {
+        return view('Pages.Auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        // login via nim,nip,email,username
+        $user = User::where('email', $request->username)
+            ->orWhere('name', $request->username)
+            ->orWhereHasMorph(
+                'userable',
+                [Mahasiswa::class, Dosen::class],
+                function ($q, $type) use ($request) {
+                    $column = $type === Mahasiswa::class ? 'npm' : 'nip';
+
+                    $q->where($column, $request->username);
+                }
+            )
+            ->first();
+
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            Auth::login($user);
+            $request->session()->regenerate();
+            return redirect()->to('/');
+        } else {
+            return back()->withErrors([
+                'loginFailed' => 'User belum terdaftar, silahkan hubungi Admin!',
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
-use App\Http\Controllers\TestingTwilloController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\TestingTwilloController;
+use App\Http\Controllers\Dashboard\DashboardController;
 
 Route::get('',[TestingTwilloController::class,'index']);
 
+// Route::group
+
+Route::controller(AuthController::class)->group(function () {
+    Route::middleware(['guest'])->group(function () {
+        Route::get('/login', 'index')->name('login');
+        Route::post('/login', 'login')->name('postlogin');
+    });
+    Route::post('/logout', 'logout')->middleware(['auth'])->name('logout');
+});
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
