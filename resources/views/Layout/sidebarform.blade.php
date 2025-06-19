6cb26d441738273caa331792a6722344b4c697bd
SatrioBagasPangestu <navi.squishy.23@gmail.com>
2025-06-19 20:07:32 +0700

    ♻️ Refactor: Code Modul #1 - Change form to layout side bar form for reuseable

---
+++ resources/views/Layout/sidebarform.blade.php
@@ -0,0 +1,20 @@
+    <div class="fixed-plugin">
+        <div class="card shadow-lg ">
+            <div class="card-header pb-0 pt-3 ">
+                <div class="float-start">
+                    <h5 class="mt-3 mb-0" id='titleSidebarForm'>@yield('titleSidebarForm')</h5>
+                    <p id='sub-titleSidebarForm'>@yield('sub-titleSidebarForm')</p>
+                </div>
+                <div class="float-end mt-4">
+                    <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+                        <i class="fa fa-close">x</i>
+                    </button>
+                </div>
+                <!-- End Toggle Button -->
+            </div>
+            <hr class="horizontal dark my-1">
+            <div class="card-body pt-sm-3 pt-0">
+                @yield('contentSidebarForm')
+            </div>
+        </div>
+    </div>
