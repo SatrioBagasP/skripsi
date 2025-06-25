5e339f22b792aa999aeb6719545f1c20adac7592
SatrioBagasPangestu <navi.squishy.23@gmail.com>
2025-06-25 22:07:00 +0700

    🔧 Fix: Bug Modul #1 - Change relation morp from kemahasiswaan to unit kemahasiswaan on login

--- app/Models/Testing.php
+++ app/Models/Testing.php
@@ -6,28 +6,28 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 
-class User extends Authenticatable
-{
-    protected $table = 'users';
-    protected $fillable = [
-        'name',
-        'roles_id',
-        'userable_id',
-        'userable_type',
-        'email',
-        'password',
-    ];
+// class User extends Authenticatable
+// {
+//     protected $table = 'users';
+//     protected $fillable = [
+//         'name',
+//         'roles_id',
+//         'userable_id',
+//         'userable_type',
+//         'email',
+//         'password',
+//     ];
     
-    public function proposal(){
-        return $this->hasMany(Proposal::class,'user_id','id');
-    }
+//     public function proposal(){
+//         return $this->hasMany(Proposal::class,'user_id','id');
+//     }
 
-    public function roles(){
-        return $this->belongsTo(Roles::class,'roles_id','id');
-    }
+//     public function roles(){
+//         return $this->belongsTo(Roles::class,'roles_id','id');
+//     }
 
-    public function userable()
-    {
-        return $this->morphTo();
-    }
-}
+//     public function userable()
+//     {
+//         return $this->morphTo();
+//     }
+// }
