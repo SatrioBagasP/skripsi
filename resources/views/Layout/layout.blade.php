141b5e4ed5bb2a734fb62c624dd280882d2e5f7d
SatrioBagasP <navi.squishy.23@gmail.com>
2025-08-24 11:29:24 +0700

    ✨ Enhance: Add placeholder to select2 dropdown in layout

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -102,7 +102,9 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
-            $('.select2').select2();
+            $('.select2').select2({
+                placeholder: '-- Pilih Data --'
+            });
             $('#sidebarform-btn').click(function(e) {
                 $('#sidebarForm').find('input, textarea, select').val('').trigger('change');
             });
