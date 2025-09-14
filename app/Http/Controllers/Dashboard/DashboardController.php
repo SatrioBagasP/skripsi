04e63ea9909b9b59abe473458c4c4afe23b4a8f0
SatrioBagasP <navi.squishy.23@gmail.com>
2025-09-14 19:29:55 +0700

    ✨ Feat: Added feat dashboard agenda

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -2,12 +2,52 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Models\Proposal;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
-class DashboardController extends Controller {
+class DashboardController extends Controller
+{
 
-    public function index(){
-        return view('Pages.Dashboard.index');
+    public function index()
+    {
+
+        $data = DB::table('proposal')
+            ->join('unit_kemahasiswaan as uk', 'proposal.unit_id', '=', 'uk.id')
+            ->select(
+                'proposal.id',
+                DB::raw("
+                    CONCAT(
+                        CASE
+                            WHEN proposal.is_harian = 0 THEN
+                                CONCAT(' ', TIME_FORMAT(start_date, '%H:%i'), ' - ', TIME_FORMAT(end_date, '%H:%i'),' ')
+                            ELSE ''
+                        END,
+                        proposal.name,
+                        ' (', uk.name, ')'
+                    ) as title
+                "),
+                DB::raw("
+                    CASE WHEN proposal.is_harian = 1
+                        THEN
+                            true
+                        ELSE
+                            false
+                        END
+                    as allDay"),
+                'start_date as start',
+                DB::raw("
+                    CASE
+                        WHEN proposal.is_harian = 1
+                            THEN DATE_ADD(end_date, INTERVAL 1 DAY)
+                        ELSE end_date
+                    END as end
+                "),
+            )
+            ->where('proposal.status', 'Accepted')
+            ->get()
+            ->toArray();
+
+        return view('Pages.Dashboard.index', compact('data'));
     }
-
 }
--- resources/views/Pages/Dashboard/index.blade.php
+++ resources/views/Pages/Dashboard/index.blade.php
@@ -1,4 +1,4 @@
-@extends('Layout.layout')
+{{-- @extends('Layout.layout')
 
 @section('pages', 'Dashboard')
 
@@ -6,8 +6,93 @@
 
 @section('content')
 
-    <div>
-        asdasd
+    <div class="card">
+        <div class="card-body">
+            <div id='calendar'></div>
+        </div>
+
     </div>
 
+
 @endsection
+
+@push('js')
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.js'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth'
+            });
+            calendar.render();
+        });
+    </script>
+@endpush --}}
+
+@extends('Layout.layout')
+
+@section('pages', 'Dashboard')
+@section('title', config('app.name') . ' | Dashboard')
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <div class='d-flex justify-content-between align-items-center'>
+                <div>
+                    <h1>Agenda Unit Kemahasiswaan ITATS</h1>
+                </div>
+
+
+            </div>
+            <div id="calendar"></div>
+        </div>
+    </div>
+@endsection
+
+@push('js')
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.js"></script>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/locales-all.global.min.js'></script>
+    <script>
+        (function() {
+
+            let dataSet = @json($data ?? []);
+
+            $(document).ready(function() {
+                var calendarEl = document.getElementById('calendar');
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+
+                    initialView: 'dayGridMonth',
+                    timeZone: 'Asia/Jakarta',
+                    locale: 'id',
+
+                    displayEventTime: false,
+                    displayEventEnd: false,
+                    dayMaxEvents: 3,
+                    aspectRatio: 2,
+
+                    headerToolbar: {
+                        left: '',
+                        center: 'title'
+                    },
+                    footerToolbar: {
+                        right: 'today prev,next'
+                    },
+
+                    // eventTimeFormat: {
+                    //     hour: '2-digit',
+                    //     minute: '2-digit',
+                    //     hour12: false,
+                    // },
+
+                    events: dataSet,
+
+                    nextDayThreshold: '00:00:00'
+
+                });
+
+                calendar.render();
+
+            });
+        })()
+    </script>
+@endpush
